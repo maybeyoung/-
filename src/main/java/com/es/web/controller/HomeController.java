af5022789707fad462416feb6f7dd03d984fe551
wsj <stephen0607@qq.com>
2018-05-11 00:03:35 +0800

    添加是静态页面修改不用热启动的代码以及HomeController的一些跳转代码

--- src/main/java/com/es/web/controller/HomeController.java
+++ src/main/java/com/es/web/controller/HomeController.java
@@ -17,4 +17,26 @@ public class HomeController {
         model.addAttribute("name","测试");
         return "index";
     }
+
+    @GetMapping("/404")
+    public String notFoundPage() {
+        return "404";
+    }
+
+    @GetMapping("/403")
+    public String accessError() {
+        return "403";
+    }
+
+    @GetMapping("/500")
+    public String internalError() {
+        return "500";
+    }
+
+    @GetMapping("/logout/page")
+    public String logoutPage() {
+        return "logout";
+    }
+
+
 }
